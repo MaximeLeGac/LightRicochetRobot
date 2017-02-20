51aee88853035ed88d77cffecea4975c6de460d3
vanbleusa <a.vanbleus@gmail.com>
2017-02-20 19:21:11 +0100

    externalisation classes avec commentaires + MAJ création Map

--- RicochetMap.py
+++ RicochetMap.py
@@ -1,42 +1,34 @@
-# Classe Case
-# Représente une case dans la Map avec ses coordonnées et la présence d'obstacle(s)
-# à sa position
-class Case():
-    def __init__(self, x =0, y =0, murHaut =Flase, murBas =False, murGauche =False, murDroite =False):
-        self.x = x
-        self.y = y
-        self.murHaut = murHaut
-        self.murBas = murBas
-        self.murGauche = murGauche
-        self.murDroite = murDroite
-
-
-# Classe Robot
-# Représente un robot sur la map avec ses coordonnées à un instant T et sa couleur
-class Robot ():
-    def __init__(self, x =0, y =0, couleur =0, passages =[0]):
-        self.x = x
-        self.y = y
-        self.couleur = couleur
-        self.passages = passages
-
-
-# Classe Map
-# Représente une carte
-class Map():
-    def __init__(self, size =0):
-        self.lineList = [size][size]
-        self.size = size
-
-    def appenLine(self, line):
-        self.lineList.append(line)
+import Map
+import Robot
+import Case
+import random
 
 
 # Méthode de génération de la carte
+# size  : Taille de la Map
 def generateMap(size):
-    map = Map(8)
-    # Générer la position de départ du robot
-    # Générer la position d'arrivée
-    # Remplir la map de cases
-    # Assigner les obstacles
+    
+    # Initialise la map
+    map = Map(size)
+    
+    # Génère la position d'arrivée
+    map.setArrival()
+
+    # Remplit la map de cases
+
+    # Assigne les obstacles
+
     return map
+
+
+# Méthode d'initialisatio d'un robot
+# size  : Taille de la Map
+# color : Couleur du Robot
+def initRobot(size, color):
+
+    # Génère les coordonnées de départ du robot
+    x = random.randint(0, size)
+    y = random.randint(0, size)
+
+    # Initialise le robot
+    return Robot(x, y, color)
---
+++ Robot.py
@@ -0,0 +1,20 @@
+# Classe Robot
+# Représente un robot sur la map avec ses coordonnées à un instant T et sa couleur
+class Robot():
+    
+    # Méthode d'initialisation
+    def __init__(self, x =0, y =0, couleur =0):
+        
+        # Définit la position du robot sur la carte
+        self.x = x
+        self.y = y
+
+        # Initialise la couleur du robot
+        self.couleur = couleur
+
+        # Initialise la liste des directions prises par le robot
+        # H = Haut
+        # B = Bas
+        # G = Gauche
+        # D = Droite
+        self.passages = [0]
