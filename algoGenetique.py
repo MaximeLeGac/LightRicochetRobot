fab78beb14ad5f56d04ac28c494a4987e1d284cb
nirv-44 <vivion.nicolas@laposte.net>
2017-02-20 19:17:45 +0100

    prepa function algo genetique

--- algoGenetique.py
+++ algoGenetique.py
@@ -1,13 +1,33 @@
 import RicochetMap
 
+CONST_TAILLE_POPULATION = 10
 
 def createrobot():
 	robot = GetRobot("bleu")
+	init(robot)
 
 
-def populationDuRobot():
-	
+def init(robot):
+	for i in range(CONST_TAILLE_POPULATION):
+		robot.passages = deplacementRandom()
+
+
+
 
 def deplacementRandom():
+	
+	return 0
 
 
+def evaluation():
+	return 0
+
+
+
+def croiser():
+	return 0
+
+
+
+def muter():
+	return 0
