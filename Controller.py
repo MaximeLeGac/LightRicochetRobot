5d2c525f7a3b9949e3df2e56dc96bf1bd06fc1f0
MaximeLeGac <maxime.le.gac.44@gmail.com>
2017-02-21 11:47:59 +0100

    Ajout implémentation méthode croiser

--- Controller.py
+++ Controller.py
@@ -1,10 +1,4 @@
-# Classe Individu
-# Représente un individu qui sera utilisé lors de l'exécution de l'algorithme génétique
-class Individu ():
-    def __init__(self, passages =[0], note =0, taille =0):
-        self.passages = passages
-        self.note = note
-        self.taille = taille
+
 
 
 
--- algoGenetique.py
+++ algoGenetique.py
@@ -24,16 +24,33 @@ def evaluation():
 
 
 
-def croiser(parent1, parent2):
-	# arrondit permettant de savoir si l'on va prendre le 50% au chiffre supérieur ou inférieur en cas de taille impaire d'un des parents
-	arrondit = random.randint(1, 2)
+def croiser(individu_parent1, individu_parent2):
 	
+	taille_parent1 = len(individu_parent1.passages) # Taille du premier individu parent
+	taille_parent2 = len(individu_parent2.passages) # Taille du deuxième individu parent
+
+	taille_moyenne1 = round((taille_parent1 / 2), 0)
+	taille_moyenne2 = round((taille_parent2 / 2), 0)
 	
-	
+	# Création des enfants
+	enfant1[0:taille_moyenne1] = individu_parent1[0:taille_moyenne1]
+	enfant1[taille_moyenne2+1:taille_parent2] = individu_parent2[taille_moyenne2+1:taille_parent2]
+
+
+	enfant2[0:taille_moyenne2] = individu_parent2[0:taille_moyenne2]
+	enfant2[taille_moyenne1+1:taille_parent1] = individu_parent1[taille_moyenne1+1:taille_parent1]
 	
 	return 0
 
 
 
 def muter():
+	# Par defaut, mutation rate de 5%
+	# mais dans le futur, mutation rate qui évolue en fonction du taux de réussite des individus (si taux stagne, augmentation du mutation rate)
+
+	taux_mutation = random.randint(0,100)
+	if 0 <= taux_mutation <= 5:
+		
+
+
 	return 0
