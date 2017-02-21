1352c6cedd9f5eb2954bba6f0c4c625294bbcf46
nirv-44 <vivion.nicolas@laposte.net>
2017-02-21 11:49:50 +0100

    gestion deplacement et colision

--- Controller.py
+++ Controller.py
@@ -3,10 +3,20 @@ import RicochetMap
 # Classe Individu
 # Représente un individu qui sera utilisé lors de l'exécution de l'algorithme génétique
 class Individu ():
-    def __init__(self, passages =[0], note =0, taille =0):
+    def __init__(self, passages =[0], note =0):
         self.passages = passages
         self.note = note
-        self.taille = taille
 
 
 
+def superalgo():
+# on rempli de base la population
+	init()
+	generation = 10
+	while generation > i: 
+		# evaluation
+
+		#Croisement
+
+		# muter
+		i = i + 1
--- algoGenetique.py
+++ algoGenetique.py
@@ -11,10 +11,72 @@ def createrobot():
 	init(robot, carte)
 
 
-def init(robot):
+def init(robot, carte):
 	for i in range(CONST_TAILLE_POPULATION):
-		for u in range()
-			robot.passages[u] = deplacementRandom()
+		tailleUnePopulation = random.randint(0, 15)
+		for u in range(tailleUnePopulation)
+			robot.passages[u] = gestionDeplacement(carte)
+			
+
+			 
+
+# Ici on se deplacement sur la carte
+# On gere les collisions, les demis tour etc...
+def gestionDeplacement(carte):
+	deplaRandom = deplacementRandom()
+	positionTempo = []
+	positionCourante = [] #Position du robot x, y
+	stop = 0
+	# Tant que nos position son différente nous continuons a avancer (pour faire la ligne complete)
+	while stop == 0:
+		# Si notre position reste la meme alors nous avons taper un mur et on sort de la boucle 
+ 		positionCourante = gestionCollision(positionCourante, deplaRandom, carte)
+ 		if(positionTempo != positionCourante):
+ 			positionTempo = positionCourante
+ 		else:
+ 			stop = 1
+
+ 	# ici on finit par return la lettre correspondant au deplacement 
+ 	if(deplaRandom[0] == 0 and deplacementRandom[1] == 1):
+    	return "H"
+    elif(deplaRandom[0] == 0 and deplacementRandom[1] == -1):
+    	return "B"
+    elif(deplaRandom[0] == 1 and deplacementRandom[1] == 0):
+    	return "D"
+    elif(deplaRandom[0] == -1 and deplacementRandom[1] == 0):
+		return "G"
+ 	else:
+    	return "Error"
+
+
+############################################""Penser a faire les colission robot
+############################################""Penser a faire les colission robot
+############################################""Penser a faire les colission robot
+
+# Verifie si mur sur notre deplacement
+# positionRobot : position du robot initial
+# deplacement : deplacement qu'il doit produire
+# Carte : carte avec les mur
+# return position
+def gestionCollision(positionRobot, deplacement, carte):
+	# ici gerer la carte pour les collisions
+	tailleCarte = len(carte) - 1
+
+	# Pour la carte on prend la case 0
+	if(positionRobot[0] >= 0 and positionRobot[0] < tailleCarte):
+		positionRobot[0] = positionRobot[0] + deplacement[0]
+	elif(positionRobot[1] >= 0 and positionRobot[1] < tailleCarte):
+		positionRobot[1] = positionRobot[1] + deplacement[1]
+	else:
+		# rien on renvoi donc les meme valeurs (sa veut dire qu'on sort de la carte)
+
+############################################""Penser a faire les colission MUR
+############################################""Penser a faire les colission MUR
+############################################""Penser a faire les colission MUR
+############################################""Penser a faire les colission MUR
+############################################""Penser a faire les colission MUR
+
+	return positionRobot
 
 
 
@@ -27,15 +89,15 @@ def init(robot):
 def deplacementRandom():
     ran = random.randint(1, 4)
     if(ran == 1):
-    	return "H"
+    	return position[0, 1] 	# H
     elif(ran == 2):
-    	return "B"
+    	return position[0, -1] 	# B
     elif(ran == 3):
-    	return "D"
+    	return position[1, 0] 	# D
 	elif(ran == 4):
-		return "G"
+		return position[-1, 0] 	# G
 	else:
-		return "H"
+    	return position[0, 1] 	# H
 
    
 
@@ -45,13 +107,7 @@ def evaluation():
 
 
 
-def croiser(parent1, parent2):
-	# arrondit permettant de savoir si l'on va prendre le 50% au chiffre supérieur ou inférieur en cas de taille impaire d'un des parents
-	arrondit = random.randint(1, 2)
-	
-	
-	
-	
+def croiser():
 	return 0
 
 
