896f4d62e279cf346006a0a65beb6b5cd4da9f98
AllanV <a.vanbleus@gmail.com>
2017-05-15 10:26:32 +0200

    COR merge

--- controller/Controller.py
+++ controller/Controller.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 def superalgo():
 # on rempli de base la population
 	init()
@@ -14,4 +12,3 @@ def superalgo():
 		i = i + 1
 		
 # ====================================================
->>>>>>> 6a6e68e8e36c1ee8ae25e6ad145f203fb8abab5b
